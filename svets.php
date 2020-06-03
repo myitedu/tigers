4521e2f35bb6e61f674c1853d511fb4f4d6c924f
Barno Khudoyberganova <praskovyawukina@gmail.com>
2020-06-03 19:47:11 -0400

    I just changed my file

--- svets.php
+++ svets.php
@@ -3,3 +3,4 @@ echo"Hello from Svet!";
 echo"hr";
 echo"I started to learn GIT";
 echo"Hello team, whats news";
+echo"I AM INTERESTING TO LEARN IT";
--- zuhra.php
+++ zuhra.php
@@ -3,3 +3,4 @@ echo "Hello from Zuhra!";
 echo "<hr>";
 echo "I started to learn GIT";
 echo "Hi Zuxra";
+
