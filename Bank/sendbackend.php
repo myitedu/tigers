afb670ea890e8f8e4350741a1621dc08dd74b0c5
Otajon Burkhonov <otajonburxonov99@gmail.com>
2020-06-06 12:16:32 -0400

    i am cheaking my files

--- Bank/sendbackend.php
+++
@@ -1,5 +0,0 @@
-<?php
-$first_name = $_POST['first_name']??null;
-$msg = "Thank you for using Send Money Version and if Receiver receive money will let You know.";
-header("Location: final.php?msg=".$msg);
-die;
