57092a9798730d4d78bb3fe14b493b1af38bf5ee
Jon Toshmatov <jontoshmatov@yahoo.com>
2020-06-05 21:14:39 -0400

    giving a demo to Svet

--- jon.php
+++ jon.php
@@ -5,3 +5,22 @@ echo "I started to learn GIT";
 echo "<hr>";
 echo "It is 12 AM";
 echo "I Love learning Programming";
+
+//Things we want to look at today:
+//Git add .
+//Git commit
+//Git push
+
+//Step 1:
+//Before you do anything
+# git pull origin master
+//Make your changes, write new codes, create new files and so on.
+//After you are done with working.
+//Let's save your changes in the cloud.
+# git add .
+# git commit -am "I completed my html or css"
+# git pull origin master
+# if there is an error or long message then
+# git add .
+# git commit -am "fixing the merge conflict"
+# git push origin master
