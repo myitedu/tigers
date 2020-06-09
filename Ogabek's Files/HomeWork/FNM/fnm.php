3f0344e82d9f18e75cdc95d0cdfa945cdf811822
ogabek467 <ogabeka03@gmail.com>
2020-06-09 15:11:29 -0400

    I am pushing HomeWork

--- Ogabek's Files/HomeWork/FNM/fnm.php
+++ Ogabek's Files/HomeWork/FNM/fnm.php
@@ -12,17 +12,80 @@
 	<script src="/js/bootstrap.js"></script>
 </head>
 <body>
-<div id="container-fluid">
-	<div id="top_div">
-		<form method="get">
+
+<!--- Top Menu --->
+
+<div id="top_div">
+	<form method="get">
 		<a href="#">HOME</a>
 		<label>Search:</label>
 		<input type="search" size="50" name="keyword" placeholder="Search FNM" value="" required>
-				<button type="submit" class="btn btn-success"><i class="fa fa-search"></i></button>
+		<button type="submit" class="btn btn-success"><i class="fa fa-search"></i></button>
 		<a href="#">HOME</a>
-		</form>
-	</div>
+	</form>
+</div>
 
+<!--- Hole Div --->
+
+<div id="container-fluid">
+	
+	<!--- First Box --->
+		
+	<div id="box_1">
+		<a href="https://www.amazon.com/dp/B085SW2PFQ">
+		<img class="img_1" src=https://images-na.ssl-images-amazon.com/images/G/01/shazam/HOCO-S2-01837-IMDBStaticBillboard-970x250-POST-Final-en-US-HO-PVD4582-63821-8c424fb9-ff42-4b26-80a2-abee8f7f623c.jpg" alt="">
+		</a>
+	</div>
+	<hr>
+	
+	<!--- Box_2 Div --->
+	
+	<div class="box_2">
+	  <a href="#">
+		<img class="img_2" src="https://m.media-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@.jpg" alt="">
+	  </a>
+	</div>
+	
+	<!--- Box_3 Div and Table --->
+	<!---
+	"id":"tt4126476"
+"title":"After "
+"year":"2019"
+"length":"1h 45min"
+"rating":"5.4"
+"rating_votes":"22887"
+   ---->
+	<div class="box_3">
+		<table>
+			<tr><td style=" width:50%;">Title:</td><td></td></tr>
+			<tr><td>Year:</td><td></td></tr>
+			<tr><td>Length:</td><td></td></tr>
+			<tr><td>Rating:</td><td></td></tr>
+			<tr><td>Rating Vote:</td><td></td></tr>
+			<tr><td>ID:</td><td></td></tr>
+		</table>
+	</div>
+	<div class="box_4">
+		<a href="#">
+		<h4>Titanic</h4>
+		</a>
+		<hr>
+		<p>"A young woman falls for a guy with a dark secret and the two embark on a rocky relationship. Based on the novel by Anna Todd."</p>
+	</div>
+</div>
+<div id="bottom_div">
+  <div class="box_6">
+	  <a href="">Get the FNM App</a>
+	  <a href="">Help</a>
+	  <a href="">Site Index</a>
+	  <a href="">FNMPro</a>
+	  <a href="">FNM TV</a>
+	  <a href="">Box Office Mojo</a>
+	  <a href="">FNM Developers</a>
+  </div>
+	<div class="box_7">
+		<p>2000-2020 by FNM.com, Inc.</p>
+	</div>
 </div>
 <script src="/js/jquery.min.js"></script>
 <script src="/js/bootstrap.min.js"></script>
