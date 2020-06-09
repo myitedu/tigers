e05f4bb8c953eca5a4a4c6eac4081b1b5799e776
ogabek467 <ogabeka03@gmail.com>
2020-06-09 01:38:14 -0400

    I am pushing

---
+++ Ogabek's Files/HomeWork/FNM/fnm.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>The Top Movies in The Our FNM</title>
+	<link rel="stylesheet" href="fnmcss.css">
+	<link rel="stylesheet" href="/css/bootstrap.css">
+	<link rel="stylesheet" href="/fontawesome/css/all.css">
+	<script src="/js/bootstrap.js"></script>
+</head>
+<body>
+<div id="container-fluid">
+	<div id="top_div">
+		<form method="get">
+		<a href="#">HOME</a>
+		<label>Search:</label>
+		<input type="search" size="50" name="keyword" placeholder="Search FNM" value="" required>
+				<button type="submit" class="btn btn-success"><i class="fa fa-search"></i></button>
+		<a href="#">HOME</a>
+		</form>
+	</div>
+
+</div>
+<script src="/js/jquery.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+</body>
+</html>
