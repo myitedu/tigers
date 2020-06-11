04edb2c54b236986db3a7f92a590f9d71333a71f
Otajon Burkhonov <otajonburxonov99@gmail.com>
2020-06-10 22:49:36 -0400

    Homework

---
+++ Otajon/1-Challange.Integers/Integers.php
@@ -0,0 +1,139 @@
+<?php
+function getMax($num1, $num2, $num3)
+{
+   if($num1>$num2)
+   {
+      if($num1>$num3){
+          echo" Maximum number is $num1";
+      }
+      else if ($num1<$num3){
+          echo "Maximum number is $num3";
+      }
+      else{
+          echo "Maximum number is $num2";
+      }
+   }
+   else  if($num1<$num2)
+   {
+       if($num1>$num3){
+           echo" Maximum number is $num2";
+       }
+       else if ($num1<$num3 && $num2<$num3){
+           echo "Maximum number is $num3";
+       }
+       else{
+           echo "Maximum number is $num2";
+       }
+   }
+   else if($num1=$num2 ){
+       if($num2>$num3) {
+           echo " Maximum number is $num2";
+       }
+       else if($num2<$num3)
+       {
+           echo "Maximum number is $num3";
+       }
+       else{
+           echo" All numbers are equil";
+       }
+   }
+}
+function getMin($num1,$num2,$num3)
+{
+    if($num1>$num2)
+    {
+        if($num2>$num3)
+        {
+            echo "Minimum number is $num3";
+        }
+        else if ($num2<$num3){
+            echo "Minimum number is $num2";
+        }
+        else{
+            echo "Minimum number is $num2";
+        }
+    }
+    else if($num1<$num2)
+    {
+        if($num1<$num3)
+        {
+            echo"Minimum number is $num1";
+        }
+        elseif($num1>$num3){
+            echo"Minimum number is $num3";
+        }
+        else{
+            echo"Minimum number is $num1";
+        }
+    }
+    else if($num1=$num2){
+        if($num1>$num3)
+        {
+            echo"Minimum number is $num3";
+        }
+        else if($num1<$num3)
+        {
+            echo"Minimum number is $num1";
+        }
+        else{
+            echo "All numbers are equil";
+        }
+    }
+}
+function getMiddle($num1,$num2,$num3)
+{
+    if($num1>$num2)
+    {
+        if($num1>$num3 && $num2<$num3)
+        {
+            echo" Middle number is $num2";
+        }
+        else if ($num1>$num3 && $num2>$num3 )
+        {
+            echo "Middle number is $num2";
+        }
+        else if( $num1<$num3){
+            echo "Middle number is $num1";
+        }
+        else if ($num1=$num3){
+            echo "We have two same number $num3 and smallar number is $num2";
+        }
+        else if($num2=$num3)
+        {
+            echo "We have two same number $num2 and bigger number is $num1";
+        }
+    }
+    else if($num1<$num2)
+    {
+        if ($num1<$num3 && $num2<$num3)
+        {
+            echo "Minimum number is $num2";
+        }
+        else if($num1<$num3 && $num2>$num3)
+        {
+            echo "Minimum number is $num3";
+        }
+        else if($num1>$num3)
+        {
+            echo"Minimum number is $num1";
+        }
+        else if($num1=$num3)
+        {
+            echo "We have two same number $num1 and bigger number is $num2";
+        }
+        else{
+            echo "We have two same number $num2 and smoller number is $num1";
+        }
+    }
+    else {
+        echo "All numbers are same";
+    }
+}
+$a=rand(1,100);
+$b=rand(1,100);
+$c=rand(1,100);
+
+echo getMax($a, $b, $c); echo"<br>";
+echo getMin($a, $b ,$c); echo"<br>";
+echo getMiddle($a, $b ,$c); echo"<br>";
+?>
